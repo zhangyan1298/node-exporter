67f0fab0c50ccc3317a061adceb1fe51faf32046
zhangyan1298 <38239794+zhangyan1298@users.noreply.github.com>
2018-06-07 14:10:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-node_exporter --path.procfs=/usr/proc --path.sysfs=/usr/sys  \
---collectors.enabled="diskstats,filefd,filesystem,loadavg,meminfo,netdev,stat,time,uname,vmstat"
+node_exporter --path.procfs=/usr/proc --path.sysfs=/usr/sys  --collectors.enabled="diskstats,filefd,filesystem,loadavg,meminfo,netdev,stat,time,uname,vmstat"
